f706c952416b9113361775427e534e8abb963a29
Leticia Andrade <leticiaae@hotmail.com.br>
2022-09-25 13:13:11 -0300

    Mudanças

--- activity01/bankingSystem/log.java
+++ activity01/bankingSystem/log.java
@@ -1,23 +1,35 @@
 package activity01.bankingSystem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Log {
-        private static Log instanciaUnica;
-      
-        public static Log getInstance() {
-            if (instanciaUnica == null) {
-                synchronized (Log.class) {
-                    if (instanciaUnica==null) {
-                        instanciaUnica= new Log();
-                    }
-                }
-            }
-            return instanciaUnica;
-            }
-            private Log() {
+
+        private static Log instanciaUnica = null;
+        private List<Integer> numeroContas = new ArrayList<>();
+        private Log() {
             
-            }
-          
         }
+        public static Log getInstance() {
+            if (instanciaUnica == null) 
+                instanciaUnica= new Log();
+               return instanciaUnica;
+            }
+        public void addnumeroConta(int numeroConta) {
+            numeroContas.add(numeroConta);
+        }
+        public List<Integer> getNumeroContas() {
+            List<Integer> retorno = new ArrayList<>();
+            retorno.addAll(numeroContas);
+            return retorno;
+        }
+        public int getNumeroConta(int index) {
+            return numeroContas.get(index);
+        }
+          public int setNumeroConta (int index, int numeroConta) {
+            return numeroContas.set(index, numeroConta);
+          }
+}
     
     
     
--- activity01/bankingSystem/operacao.java
+++ activity01/bankingSystem/operacao.java
@@ -3,10 +3,18 @@ package activity01.bankingSystem;
 public class operacao {
       
         public static void main(String[] args) {
-                Log log1 = Log.getInstance();
+                Log log = Log.getInstance();
+                log.addnumeroConta(12345);
+                log.addnumeroConta(56788);
+                log.addnumeroConta(967545);
+                System.out.println(log.getNumeroConta(2));
+               
                 Log log2 = Log.getInstance();
-                System.out.println(log1.hashCode());
-                System.out.println(log2.hashCode());
+                System.out.println(log2.getNumeroConta(0));
+                System.out.println(log.hashCode());
+                System.out.println(log2.hashCode());  
+                log2.setNumeroConta(0, 3344);
+                System.out.println(log2.getNumeroConta(0));
 
         }
     }
