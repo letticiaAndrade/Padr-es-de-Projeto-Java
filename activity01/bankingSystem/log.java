f73a67f72ac1e7a45bb0bb1c47cba7113d05f78e
Leticia Andrade <leticiaae@hotmail.com.br>
2022-09-25 03:59:41 -0300

    Andamento da atividade 1

--- activity01/bankingSystem/log.java
+++ activity01/bankingSystem/log.java
@@ -1,6 +1,24 @@
 package activity01.bankingSystem;
 
-public class log {
+public class Log {
+        private static Log instanciaUnica;
+      
+        public static Log getInstance() {
+            if (instanciaUnica == null) {
+                synchronized (Log.class) {
+                    if (instanciaUnica==null) {
+                        instanciaUnica= new Log();
+                    }
+                }
+            }
+            return instanciaUnica;
+            }
+            private Log() {
+            
+            }
+          
+        }
     
     
-}
+    
+    
---
+++ activity01/bankingSystem/operacao.java
@@ -0,0 +1,14 @@
+package activity01.bankingSystem;
+
+public class operacao {
+      
+        public static void main(String[] args) {
+                Log log1 = Log.getInstance();
+                Log log2 = Log.getInstance();
+                System.out.println(log1.hashCode());
+                System.out.println(log2.hashCode());
+
+        }
+    }
+
+
